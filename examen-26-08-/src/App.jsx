a3c3efdb42d12c579237ac3df6a9d41ac6bf237a
John Doe <johndoe@example.com>
2024-08-26 10:45:09 -0300

    examen-26-08-

--- examen-26-08-/src/App.jsx
+++ examen-26-08-/src/App.jsx
@@ -2,13 +2,64 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resultado, setResultado] = useState(0);
+  const [inputValor, setInputValor] = useState(0);
+  
+  const inputValue = ((even)=>{
+    setInputValor(even.target.value);
+    console.log(setInputValor);
+  });
+
+  const calcular = (operacion) => {
+    const valorActual = parseFloat(inputValue);
+    let resultado;
+
+    switch (operacion) {
+      case 'sumar':
+        resultado = valorActual + valorActual;
+        break;
+      case 'restar':
+        resultado = valorActual - valorActual;
+        break;
+      case 'multiplicar':
+        resultado = valorActual * valorActual;
+        break;
+      case 'dividir':
+        resultado = valorActual / valorActual;
+        break;
+      default:
+        return;
+    }
+
+    setInputValor(resultado.toNumber());
+  };
 
   return (
     <>
-     
+    <div>
+      <input className='input' onChange={inputValue} type="number" value={setInputValor} />
+    </div>
+    <div className='numeros'>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+    </div>
+    <div className='operaciones'>     
+      <button className='sumar' onChange={() => calcular('sumar')}>sunar</button>
+      <button className='dividir' onChange={() => calcular('dividir')}>dividir</button>
+      <button className='restar' onChange={() => calcular('restar')}>restar</button>
+      <button className='multiplicar' onChange={() => calcular('multiplicar')}>multiplicar</button>
+      <button className='mostrar resultado' onChange={() => calcular('sumar')}>mostrar resultado</button>
+    </div>
+
     </>
   )
 }
 
-export default App
+export default App;
